d030f81b734cc1c5e9165cc47084df631acd6fb9
FranklysG <franklys@appmake.com.br>
2021-03-06 11:24:44 -0300

    adjusts

---
+++ app/config-s/app.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'host'  =>  "127.0.0.1",
+    'port'  =>  "",
+    'name'  =>  "gandul96_gandula",
+    'user'  =>  "gandul96_master",
+    'pass'  =>  "gandul@2021.",
+    'type'  =>  "mysql",
+    'prep'  =>  "1"
+];
