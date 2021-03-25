821b11c635a6f3a4479d2f0cc53475af99f91e5d
FranklysG <franklys@appmake.com.br>
2021-03-25 20:36:48 -0300

    adjusts in the model champion league

---
+++ app/model/view/ViewTableOddChampionsLeague.class.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * ViewTableOddChampionsLeague Active Record
+ * @author  <your-name-here>
+ */
+class ViewTableOddChampionsLeague extends TRecord
+{
+    const TABLENAME = 'view_table_odd_champions_league';
+    const PRIMARYKEY= 'soccer_team_id';
+    const IDPOLICY =  'max'; // {max, serial}
+    
+    
+    /**
+     * Constructor method
+     */
+    public function __construct($id = NULL, $callObjectLoad = TRUE)
+    {
+        parent::__construct($id, $callObjectLoad);
+        parent::addAttribute('slug');
+        parent::addAttribute('acron');
+        parent::addAttribute('pts');
+        parent::addAttribute('j');
+        parent::addAttribute('v');
+        parent::addAttribute('e');
+        parent::addAttribute('d');
+        parent::addAttribute('gp');
+        parent::addAttribute('gc');
+        parent::addAttribute('sg');
+        parent::addAttribute('ap');
+        parent::addAttribute('odds');
+    }
+
+
+}
